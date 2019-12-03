6cd97c2c5abc39da1d5bcc83e2b1bb5ba1fc9871
EdenReuveni <57111926+EdenReuveni@users.noreply.github.com>
2019-12-03 21:03:53 +0200

    adding documentation

--- Ex1/ComplexFunction.java
+++ Ex1/ComplexFunction.java
@@ -1,51 +1,118 @@
 package Ex1;
-
+/**
+ * this class represents a complex function that have a left and right branches, with an operator in the middle.
+ * each branch can be a complex function by itself.
+ * @author Eden Reuveni
+ * @author Noa Aizer
+ *
+ */
 public class ComplexFunction implements complex_function{
 	private function leftFunc;
 	private function rightFunc;
 	private Operation op;
 
-
-	public ComplexFunction(Operation new_op,function left, function right) {
-		// TODO Auto-generated constructor stub
-		constructorHelper(left, right, new_op);
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type Operator and is the first of the variables
+	 * @param op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
+	public ComplexFunction(Operation op,function left, function right) {
+		constructorHelper(left, right, op);
 	}
-	public ComplexFunction(function left,Operation new_op, function right) {
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type Operator and is the second of the variables
+	 * @param op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
+	public ComplexFunction(function left,Operation op, function right) {
 		// TODO Auto-generated constructor stub
-		constructorHelper(left, right, new_op);
+		constructorHelper(left, right, op);
 	}
-	public ComplexFunction(function left, function right, Operation new_op) {
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type Operator and is the third of the variables
+	 * @param op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
+	public ComplexFunction(function left, function right, Operation op) {
 		// TODO Auto-generated constructor stub
-		constructorHelper(left, right, new_op);
+		constructorHelper(left, right, op);
 	}
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type String and is the first of the variables
+	 * @param new_op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
 	public ComplexFunction(String new_op, function left, function right) {
 		// TODO Auto-generated constructor stub
-		constructorHelper(left, right, Operator(new_op));
+		constructorHelper(left, right, operatorBuilder(new_op));
 	}
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type String and is the second of the variables
+	 * @param new_op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
 	public ComplexFunction(function left, String new_op, function right) {
-		constructorHelper(left, right, Operator(new_op));
+		constructorHelper(left, right, operatorBuilder(new_op));
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * constructor for building a new complex function
+	 * the operator is of type String and is the third of the variables
+	 * @param new_op is the given operator 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 */
 	public ComplexFunction(function left, function right, String new_op) {
-		constructorHelper(left, right, Operator(new_op));
+		constructorHelper(left, right, operatorBuilder(new_op));
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * constructor for building a new complex function from a monom
+	 * @param monom is the given monom
+	 */
 	public ComplexFunction(Monom monom) {
 		this.leftFunc=monom;
 		this.rightFunc=null;
 		this.op=Operation.None;
 	}
+	/**
+	 * constructor for building a new complex function from a polynom
+	 * @param polynom is the given polynom
+	 */
 	public ComplexFunction(Polynom polynom) {
 		this.leftFunc=polynom;
 		this.rightFunc=null;
 		this.op=Operation.None;
 	}
+	/**
+	 * constructor to build a new complax function
+	 * each constructor is using this one to fit the given parameters to this order
+	 * (left, right, operator) 
+	 * @param left is the given left function
+	 * @param right is the given right function
+	 * @param new_op is the given operator
+	 */
 	private void constructorHelper(function left, function right, Operation new_op) {
 		this.leftFunc=left.copy();
 		this.rightFunc=right.copy();
 		this.op=new_op;	
 	}
-	private Operation Operator(String new_op) {
+	/**
+	 * function to convert a string representing an operation to its enum value
+	 * @param new_op is the given string
+	 * @return is the enum value of the operation
+	 */
+	private Operation operatorBuilder(String new_op) {
 		new_op.toLowerCase();
 		switch(new_op) {
 		case "plus":
@@ -148,7 +215,7 @@ public class ComplexFunction implements complex_function{
 				if (this.rightFunc.equals(null)) {
 					throw new RuntimeException("ERR right function should be null");
 				}
-		}
+			}
 		}
 		catch (Exception e) {
 			System.out.println("ERR right function should be null");
@@ -190,6 +257,11 @@ public class ComplexFunction implements complex_function{
 		}
 
 	}
+	/**
+	 * creates a function from a string
+	 * @param s is the given string
+	 * @return the complex function
+	 */
 	@Override
 	public function initFromString(String s) {
 		int counterForStart=0, counterForEnd=0, CounterForComma=0;
@@ -202,15 +274,15 @@ public class ComplexFunction implements complex_function{
 				CounterForComma++;
 		}
 		try {
-		if (counterForEnd>counterForStart || counterForStart>counterForEnd) {
-			throw new RuntimeException("ERR the expretion is invalid");
-		}
+			if (counterForEnd>counterForStart || counterForStart>counterForEnd) {
+				throw new RuntimeException("ERR the expretion is invalid");
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		if (CounterForComma==0 && counterForEnd==1 && counterForStart==1) {
 			String strForOp = s.substring(0, s.indexOf('('));
-			this.op=Operator(strForOp);			
+			this.op=operatorBuilder(strForOp);			
 			this.leftFunc=new Polynom(s.substring(s.indexOf('(')+1,s.indexOf(')')));
 			this.rightFunc=null;
 			ComplexFunction cf=new ComplexFunction(this.leftFunc,this.rightFunc,this.op);
@@ -218,7 +290,7 @@ public class ComplexFunction implements complex_function{
 		}
 		if (counterForStart==1 && counterForEnd==1 && CounterForComma==1) {
 			String strForOp = s.substring(0, s.indexOf('('));
-			this.op=Operator(strForOp);			
+			this.op=operatorBuilder(strForOp);			
 			this.leftFunc=new Polynom(s.substring(s.indexOf('(')+1,s.indexOf(',')));
 			this.rightFunc=new Polynom(s.substring(s.indexOf(',')+1,s.indexOf(')')));
 			ComplexFunction cf=new ComplexFunction(this.leftFunc,this.rightFunc,this.op);
@@ -226,13 +298,19 @@ public class ComplexFunction implements complex_function{
 		}
 		return initFromString(s.substring(s.indexOf('(')+1, s.lastIndexOf(')')));
 	}
-	
+	/**
+	 * copies a function
+	 * @return the copy of the asked function
+	 */
 	@Override
 	public function copy() {
 		ComplexFunction cf= new ComplexFunction(this.leftFunc.copy(), this.rightFunc.copy(), this.op);
 		return cf;
 	}
-
+	/**
+	 * checks if two Objects are logically equals
+	 * @return the boolean value of the question
+	 */
 	public boolean equals(Object obj) {
 		if (obj instanceof ComplexFunction) {
 			ComplexFunction cf= new ComplexFunction(this.leftFunc, this.rightFunc, this.op);
@@ -248,6 +326,14 @@ public class ComplexFunction implements complex_function{
 		}
 		return false;
 	}
+	/**
+	 * checks if the values of two complex functions equals- for the same index
+	 * the method equals is using this method to answer the question.
+	 * even thought this is not the perfect way to check it, but statistically it will be correct
+	 * @param cf1 is the first complex function
+	 * @param cf2 is the second complex function
+	 * @return the boolean value of the question
+	 */
 	private  boolean visualEquals(ComplexFunction cf1, ComplexFunction cf2)
 	{
 		for (double i = -100; i < 100 ; i+=Monom.EPSILON) {
@@ -256,6 +342,9 @@ public class ComplexFunction implements complex_function{
 		}
 		return true;
 	}
+	/**
+	 * prints to screen a complex function
+	 */
 	public String toString() {
 		return null;
 	}
